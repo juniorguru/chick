69624d18b7441ef99b77f97fd37fab8e5da692b6
Honza Javorek <mail@honzajavorek.cz>
2023-03-08 11:13:13 +0100

    be super sure to have #ahoj disabled for now

--- juniorguru_chick/bot.py
+++ juniorguru_chick/bot.py
@@ -19,30 +19,32 @@ def is_thread(message: discord.Message) -> bool:
     return message.type == discord.MessageType.thread_created
 
 
-async def create_ahoj_thread(message: discord.Message) -> None:
-    """Creates a thread for a message in #ahoj, ahoj_thread_handler will catch the creation"""
-    thread_name = "Ahoj {}!".format(message.author.name)
+# TODO let's start with less important channels first
+# async def create_ahoj_thread(message: discord.Message) -> None:
+#     """Creates a thread for a message in #ahoj, ahoj_thread_handler will catch the creation"""
+#     thread_name = "Ahoj {}!".format(message.author.name)
+#
+#     if is_thread(message):
+#         return
+#
+#     if message.is_system():
+#         return
+#
+#     await message.create_thread(name=thread_name)
 
-    if is_thread(message):
-        return
 
-    if message.is_system():
-        return
-
-    await message.create_thread(name=thread_name)
-
-
-async def ahoj_thread_handler(thread: discord.Thread) -> None:
-    """Called when a thread is created in #ahoj. Kuře-made threads included"""
-    message = thread.starting_message
-    member_name = message.author.name
-    thread_name = "Ahoj {}!".format(member_name)
-
-    await thread.edit(name=thread_name)
-    await message.add_reaction("👋")
-    await message.add_reaction("🐣")
-    # The thread message has to be sent last, or the API will process it before the actual users message. The reactions above work as a delay
-    await thread.send("Nazdar {}, vítej v klubu!".format(member_name))
+# TODO let's start with less important channels first
+# async def ahoj_thread_handler(thread: discord.Thread) -> None:
+#     """Called when a thread is created in #ahoj. Kuře-made threads included"""
+#     message = thread.starting_message
+#     member_name = message.author.name
+#     thread_name = "Ahoj {}!".format(member_name)
+#
+#     await thread.edit(name=thread_name)
+#     await message.add_reaction("👋")
+#     await message.add_reaction("🐣")
+#     # The thread message has to be sent last, or the API will process it before the actual users message. The reactions above work as a delay
+#     await thread.send("Nazdar {}, vítej v klubu!".format(member_name))
 
 
 async def create_pvp_thread(message: discord.Message) -> None:
@@ -87,11 +89,12 @@ async def on_message(message: discord.Message) -> None: # Message was sent
         await create_mdo_thread(message)
 
 
-@bot.event
-async def on_thread_create(thread: discord.Thread) -> None:
-    channel_name = thread.starting_message.channel.name
-    if channel_name == "ahoj":
-        await ahoj_thread_handler(thread)
+# TODO let's start with less important channels first
+# @bot.event
+# async def on_thread_create(thread: discord.Thread) -> None:
+#     channel_name = thread.starting_message.channel.name
+#     if channel_name == "ahoj":
+#         await ahoj_thread_handler(thread)
 
 
 def main():
