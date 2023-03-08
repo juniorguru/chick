6ec32d90ad05917e96b91e47697d7af7beef0e68
Honza Javorek <mail@honzajavorek.cz>
2023-03-08 21:49:34 +0100

    add http server

---
+++ juniorguru_chick/web.py
@@ -0,0 +1,25 @@
+import logging
+from datetime import datetime
+
+from aiohttp.web import Application, RouteTableDef, json_response
+
+
+LAUNCH_AT = datetime.utcnow()
+
+
+logger = logging.getLogger("chick.web")
+
+
+web = Application()
+routes = RouteTableDef()
+
+
+@routes.get('/')
+async def index(request):
+    logger.info(f'Received {request!r}')
+    return json_response({'status': 'ok',
+                          'launch_at': LAUNCH_AT.isoformat(),
+                          'uptime_sec': (datetime.utcnow() - LAUNCH_AT).seconds})
+
+
+web.add_routes(routes)
