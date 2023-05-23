d12062cf1d831d8aa03182e67a4a50fc738c5169
Honza Javorek <mail@honzajavorek.cz>
2023-05-23 13:54:00 +0200

    move helpers down

--- juniorguru_chick/bot.py
+++ juniorguru_chick/bot.py
@@ -16,43 +16,6 @@ logger = logging.getLogger("chick.bot")
 bot = commands.Bot()
 
 
-def is_thread(message: discord.Message) -> bool:
-    """Checks if given message is a system 'thread created' announcement"""
-    return message.type == discord.MessageType.thread_created
-
-
-async def fetch_starting_message(thread: discord.Thread) -> discord.Message | None:
-    """Returns the starting message of given thread"""
-    if thread.starting_message:
-        return thread.starting_message
-    try:
-        # thread.starting_message is often None although the thread
-        # has a starting message, so try to fetch it manually
-        return await thread.fetch_message(thread.id)
-    except discord.errors.NotFound:
-        return None
-
-
-async def create_thread(message: discord.Message, name_template) -> None:
-    """Creates a new thread for given message"""
-    weekday = datetime.now().weekday()
-    name = name_template.format(weekday=DAYS[weekday], author=message.author.display_name)
-    await message.create_thread(name=name)
-
-
-async def ensure_thread_name(thread: discord.Thread, name_template) -> str | None:
-    """Ensures given thread has a name"""
-    starting_message = await fetch_starting_message(thread)
-    if starting_message:
-        weekday = datetime.now().weekday()
-        name = name_template.format(weekday=DAYS[weekday], author=starting_message.author.display_name)
-        if thread.name != name:
-            await thread.edit(name=name)
-        return name
-    else:
-        return None
-
-
 @bot.event
 async def on_ready():
     if bot.user:
@@ -109,3 +72,40 @@ async def on_thread_create(thread: discord.Thread) -> None:
         await starting_message.add_reaction("<:dk:842727526736068609>")
     elif channel_name == "práce-hledám":
         await starting_message.add_reaction("👍")
+
+
+def is_thread(message: discord.Message) -> bool:
+    """Checks if given message is a system 'thread created' announcement"""
+    return message.type == discord.MessageType.thread_created
+
+
+async def fetch_starting_message(thread: discord.Thread) -> discord.Message | None:
+    """Returns the starting message of given thread"""
+    if thread.starting_message:
+        return thread.starting_message
+    try:
+        # thread.starting_message is often None although the thread
+        # has a starting message, so try to fetch it manually
+        return await thread.fetch_message(thread.id)
+    except discord.errors.NotFound:
+        return None
+
+
+async def create_thread(message: discord.Message, name_template) -> None:
+    """Creates a new thread for given message"""
+    weekday = datetime.now().weekday()
+    name = name_template.format(weekday=DAYS[weekday], author=message.author.display_name)
+    await message.create_thread(name=name)
+
+
+async def ensure_thread_name(thread: discord.Thread, name_template) -> str | None:
+    """Ensures given thread has a name"""
+    starting_message = await fetch_starting_message(thread)
+    if starting_message:
+        weekday = datetime.now().weekday()
+        name = name_template.format(weekday=DAYS[weekday], author=starting_message.author.display_name)
+        if thread.name != name:
+            await thread.edit(name=name)
+        return name
+    else:
+        return None
