6424d3095351ee00e2a94538baef21d118f23fd2
Honza Javorek <mail@honzajavorek.cz>
2023-05-29 12:05:16 +0200

    fix detecting C# and C++ (issue with word boundary)

--- tests/test_lib_intro.py
+++ tests/test_lib_intro.py
@@ -1,3 +1,4 @@
+import pytest
 from juniorguru_chick.lib.intro import choose_intro_emojis
 
 
@@ -8,3 +9,11 @@ def test_choose_intro_emojis():
     """
 
     assert '<:python:842331892091322389>' in choose_intro_emojis(message_content)
+
+
+@pytest.mark.parametrize('message_content, expected_emoji', [
+    ('základní struktury v pythonu a C# v ITnetwork', '<:csharp:842666113230045224>'),
+    ('Láká mě C++ a C#', '<:cpp:842666129071931433>'),
+])
+def test_choose_intro_emoji_edge_cases(message_content, expected_emoji):
+    assert expected_emoji in choose_intro_emojis(message_content)
