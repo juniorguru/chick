8fa509c347a75aeb7a95fca16a64433c77ee8317
Daria Grudzien <hello@dariagrudzien.com>
2023-09-11 16:33:19 +0200

    fix alternative template for thread naming

--- tests/test_lib_thread_names.py
+++ tests/test_lib_thread_names.py
@@ -43,15 +43,15 @@ def test_no_text_in_brackets():
     assert name_thread(message, name_template) == expected_name
 
 @pytest.mark.parametrize('content, expected_name', [
-    pytest.param("[eslint, nextjs]", "Past na Jana: eslint, nextjs", id="comma separated strings with spaces"),
-    pytest.param("[eslint,nextjs]", "Past na Jana: eslint, nextjs", id="comma separated strings without spaces"),
-    pytest.param("[ Java ]", "Past na Jana: Java", id="whitespaces around string"),
-    pytest.param("[:css: CSS]", "Past na Jana: :css: CSS", id="starting emoji"),
-    pytest.param("[eslint,nextjs, :css: CSS, ruby]", "Past na Jana: eslint, nextjs, :css: CSS, ruby", id="emoji in the middle"),
-    pytest.param("[ eslint ] Hello", "Past na Jana: eslint", id="brackets at the beginning")])
+    pytest.param("[eslint, nextjs]", "Objev od Jana: eslint, nextjs", id="comma separated strings with spaces"),
+    pytest.param("[eslint,nextjs]", "Objev od Jana: eslint, nextjs", id="comma separated strings without spaces"),
+    pytest.param("[ Java ]", "Objev od Jana: Java", id="whitespaces around string"),
+    pytest.param("[:css: CSS]", "Objev od Jana: :css: CSS", id="starting emoji"),
+    pytest.param("[eslint,nextjs, :css: CSS, ruby]", "Objev od Jana: eslint, nextjs, :css: CSS, ruby", id="emoji in the middle"),
+    pytest.param("[ eslint ] Hello", "Objev od Jana: eslint", id="brackets at the beginning")])
 def test_parse_message_in_brackets(content, expected_name):
     message = Message("Jana", content)
     name_template = "{weekday} objev od {author}"
-    alternative_name_template = "Past na {author}: {name}"
+    alternative_name_template = "Objev od {author}: {name}"
 
     assert name_thread(message, name_template, alternative_name_template) == expected_name
